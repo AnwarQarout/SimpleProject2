3e1130d9a761c8a6a5dec7e41adec8942bc40db1
AnwarQarout <86949586+AnwarQarout@users.noreply.github.com>
2021-07-08 10:30:48 +0300

    add test parametrization and test fixtures

--- Testing/converter_tester.py
+++ Testing/converter_tester.py
@@ -1,8 +1,36 @@
+import inspect
 import unittest
+
+
+
 from converter import *
 
+
+test_list = [("Hello there","HeLlO tHeRe"), ("hello there","hello there"), ("HELLO THERE","HELLO THERE"),
+             ("HeLlO ThErE","heLLO THERE"), ("","")]
+
+
 class TestConverter(unittest.TestCase):
+    def setUp(self):
+        self.logPoint()
+        self.string = "this is a test string"
+
 
     def test_same_result(self):
-        test_value=input("enter test text\n")
-        self.assertEqual(convert_text_using_loop_method(test_value),convert_text_using_lower_method(test_value))
+        #TEST FIXTURE - TEST SCOPE
+        self.logPoint()
+        self.assertEqual(convert_text_using_loop_method(self.string),convert_text_using_lower_method(self.string))
+
+        #TEST PARAMETRIZATION
+        for p1,p2 in test_list:
+            with self.subTest():
+                self.assertEqual(convert_text_using_loop_method(p1),convert_text_using_lower_method(p2))
+
+    def tearDown(self) -> None:
+        self.logPoint()
+
+
+    def logPoint(self):
+        currentTest = self.id().split('.')[-1]
+        callingFunction = inspect.stack()[1][3]
+        print('in %s - %s()' % (currentTest, callingFunction))
